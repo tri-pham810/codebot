7c071e764384ce319e7d1f6de4e5602c2225b479
Pham Tri <Tri.PhamMinh@mitrais.com>
2020-03-29 17:05:15 +0000

    Re-write target application

---
+++ testtarget/src/test/java/firstapp/cucumber/pom/botwritten/factories/AdminPageFactory.java
@@ -0,0 +1,49 @@
+package firstapp.cucumber.pom.botwritten.factories;
+
+import firstapp.cucumber.pom.botwritten.page.*;
+import firstapp.cucumber.pom.botwritten.page.crudlist.*;
+import com.google.inject.Inject;
+import cucumber.runtime.java.guice.ScenarioScoped;
+import java.util.Properties;
+import lombok.*;
+import org.openqa.selenium.WebDriver;
+
+@ScenarioScoped
+public class AdminPageFactory {
+
+	@Inject
+	protected WebDriver webDriver;
+
+	@Inject
+	protected Properties properties;
+
+	@Inject
+	EntitiesFishListPage entitiesFishListPage;
+	@Inject
+	EntitiesTankListPage entitiesTankListPage;
+	@Inject
+	EntitiesSpeciesListPage entitiesSpeciesListPage;
+	@Inject
+	UsersAdminListPage usersAdminListPage;
+	@Inject
+	UsersFishnaticListPage usersFishnaticListPage;
+
+
+	public CrudListPage createCrudPage(String name) throws Exception {
+		switch (name) {
+
+			case "Fish":
+				return entitiesFishListPage;
+			case "Tank":
+				return entitiesTankListPage;
+			case "Species":
+				return entitiesSpeciesListPage;
+			case "Admin":
+				return usersAdminListPage;
+			case "Fishnatic":
+				return usersFishnaticListPage;
+			default :
+				throw new Exception(String.format("Unexpected Crud list Page: %s", name));
+		}
+	}
+}
